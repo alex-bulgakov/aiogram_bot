ddde1ded72b45e748e5db2f301047a234cb778b3
alebul <master8423@gmail.com>
2023-04-13 19:59:42 +0300

    add emojy handler

--- lesson05/main.py
+++ lesson05/main.py
@@ -24,5 +24,9 @@ async def send_sticker_id(message: types.Message):
 async def give_command(message: types.Message):
     await bot.send_sticker(message.from_user.id, sticker=sticker_id)
 
+@dp.message_handler()
+async def send_emojy(message: types.Message):
+    await message.reply(message.text + '😀')
+
 if __name__ == "__main__":
     executor.start_polling(dp, on_startup=on_startup)
