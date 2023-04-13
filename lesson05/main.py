486a56e6cd686903b44c2face7accc1047e7b29a
alebul <master8423@gmail.com>
2023-04-13 19:49:13 +0300

    add stickers handler

---
+++ lesson05/main.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN
+
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)
+sticker_id = ''
+
+async def on_startup(_):
+    print('on_startup function')
+
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.answer('Start bot - <b>COOL BOT</b>', parse_mode="HTML")
+
+
+@dp.message_handler(content_types=types.ContentType.STICKER)
+async def send_sticker_id(message: types.Message):
+    global sticker_id
+    sticker_id = message.sticker.file_id
+    await message.reply(f'The sticker ID is {sticker_id}')
+
+@dp.message_handler(commands=['give'])
+async def give_command(message: types.Message):
+    await bot.send_sticker(message.from_user.id, sticker=sticker_id)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, on_startup=on_startup)
--- practice/random_symbol_bot.py
+++ practice/random_symbol_bot.py
@@ -1,3 +1,4 @@
+import string
 from random import randint
 
 from aiogram import Bot, Dispatcher, executor, types
@@ -5,22 +6,33 @@ from config import TOKEN
 
 dp = Dispatcher(Bot(TOKEN))
 DESCR = "type any words and bot answer random symbol"
-
-
-@dp.message_handler(msg: types.Message)
-async def help_command(msg: types.Message):
-    await msg.reply(msg.text[randint(0, len(msg.text))]
+counter = 0
 
 
 @dp.message_handler(commands=['description'])
 async def description_command(msg: types.Message):
-    await msg.reply(DESCR)
+    global counter
+    counter += 1
+    await msg.answer(DESCR)
+
+
+@dp.message_handler(commands=['count'])
+async def count_command(msg: types.Message):
+    global counter
+    await msg.answer(counter)
+
+
+@dp.message_handler()
+async def help_command(msg: types.Message):
+    global counter
+    counter += 1
+    text = msg.text
+    await msg.reply(string.ascii_letters[randint(0, len(text))])
+    if text.find('0') > -1:
+        await msg.reply("YES")
+    else:
+        await msg.reply("NO")
 
 
 if __name__ == "__main__":
     executor.start_polling(dp)
-
-
-
-
-
